d3a45237fc7acca7b9d5961ba8d5be30e0d283a3
pawel.wojcik7 <pawel.wojcik7@pollub.edu.pl>
2022-06-16 18:50:27 +0200

    Huffman 3

---
+++ src/Leaf.java
@@ -0,0 +1,17 @@
+
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode(callSuper = true)
+public class Leaf extends Node {
+
+    private final char character;
+
+    public Leaf(char character, int frequency) {
+        super(frequency);
+        this.character = character;
+    }
+
+}
